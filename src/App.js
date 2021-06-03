02bdac869eff671a6bc5d1f2abbcd48df92d798d
Chris Kamp <chriskamp101@gmail.com>
2021-06-03 16:12:45 +1000

    add card components and load images from pixabay API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl">Hello World</h1>
+    <div className="container mx-auto">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
     </div>
   );
 }
